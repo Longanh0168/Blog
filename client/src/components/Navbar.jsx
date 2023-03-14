131ee0ef0eb31a89debc15111a08251578748b11
Thành Long <thanhlong11a11@gmail.com>
2023-03-14 11:33:28 +0700

    4th commit

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,22 +1,21 @@
-import React from 'react'
-import { useContext } from 'react'
-import { Link } from 'react-router-dom'
-import { AuthContext } from '../context/authContext'
-import Logo from "../img/logo.png"
-
+import React from 'react';
+import { useContext } from 'react';
+import { Link } from 'react-router-dom';
+import { AuthContext } from '../context/authContext';
+import Logo from '../img/logo.png';
 
 const Navbar = () => {
-  const { currentUser, logout } = useContext(AuthContext)
+  const { currentUser, logout } = useContext(AuthContext);
 
   return (
-    <div className='navbar'>
-      <div className='container'> 
-        <div className='logo'>
+    <div className="navbar">
+      <div className="container">
+        <div className="logo">
           <Link to="/">
             <img src={Logo} alt="" />
           </Link>
         </div>
-        <div className='links'>
+        <div className="links">
           <Link className="link" to="/?cat=art">
             <h6>ART</h6>
           </Link>
@@ -36,14 +35,24 @@ const Navbar = () => {
             <h6>FOOD</h6>
           </Link>
           <span>{currentUser?.username}</span>
-          {currentUser ? <span onClick={logout}>Logout</span> : <Link className='link' to="/login">Login</Link>}
-          <span className='write'>
-            <Link className='link' to="/write">Write</Link>
+          {currentUser ? (
+            <span onClick={logout} to="/login">
+              Logout
+            </span>
+          ) : (
+            <Link className="link" to="/login">
+              Login
+            </Link>
+          )}
+          <span className="write">
+            <Link className="link" to="/write">
+              Write
+            </Link>
           </span>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
